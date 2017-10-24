0ac3e450875e8520db2986f41892968e31300dcb
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-24 16:32:14 -0400

    building out homepage template

--- wp-content/themes/yoga-mala-foundation/homepage.php
+++ wp-content/themes/yoga-mala-foundation/homepage.php
@@ -7,13 +7,12 @@ Template Name: Homepage
 <?php get_header(); ?>
 
 	<div class="heroCTA">
-		<h2><?php the_field('hero_title'); ?></h2>
-		<p><?php the_field('hero_text'); ?></p>
-		<a class="button" href="<?php the_field('hero_button_url'); ?>">
-				<?php the_field('hero_button_description'); ?> <span class="arrow"></span>
-		</a>
+		<h1><?php the_field('hero_message'); ?></h1>
 	</div>
 
+	<div class="learn">
+    <p><?php the_field('learn_more');?></p>
+	</div>
 </div> <!-- .hero -->
 
 	<div id="content">
@@ -22,13 +21,84 @@ Template Name: Homepage
 
 		    <main id="main" class="large-12 medium-12 columns" role="main">
 
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div class="mission">
 
-					<?php get_template_part( 'parts/loop', 'page' ); ?>
+						<?php the_field('mission');?>
 
-				<?php endwhile; endif; ?>
+					</div>
 
-			</main> <!-- end #main -->
+					<div id="programs" class="row">
+
+						<!-- 1st col -->
+						<div class="large-4 medium-4 columns">
+							<h2><?php the_field('programs_title');?></h2>
+							<p><?php the_field('programs_description');?></p>
+						</div>
+
+						<!-- 2nd col -->
+						<div class="large-4 medium-4 columns">
+
+							<div class="program">
+								<div class="pHeader">
+									<?php the_field('program_1_image');?>
+									<h3><?php the_field('progam_1_title');?></h3>
+								</div>
+								<p><?php the_field('program_1_text');?></p>
+							</div>
+
+							<div class="program">
+								<div class="pHeader">
+									<?php the_field('program_2_image');?>
+									<h3><?php the_field('progam_2_title');?></h3>
+								</div>
+								<p><?php the_field('program_2_text');?></p>
+							</div>
+
+							<div class="program">
+								<div class="pHeader">
+									<?php the_field('program_3_image');?>
+									<h3><?php the_field('progam_3_title');?></h3>
+								</div>
+								<p><?php the_field('program_3_text');?></p>
+							</div>
+
+						</div>
+
+						<!-- 3rd col -->
+						<div class="large-4 medium-4 columns">
+
+							<div class="program">
+								<div class="pHeader">
+									<?php //the_field('program_4_image');?>
+									<h3><?php //the_field('progam_4_title');?></h3>
+								</div>
+								<p><?php// the_field('program_4_text');?></p>
+							</div>
+
+							<div class="program">
+								<div class="pHeader">
+									<?php //the_field('program_5_image');?>
+									<h3><?php //the_field('progam_5_title');?></h3>
+								</div>
+								<p><?php //the_field('program_5_text');?></p>
+							</div>
+
+							<div class="program">
+								<div class="pHeader">
+									<?php //the_field('program_6_image');?>
+									<h3><?php //the_field('progam_6_title');?></h3>
+								</div>
+								<p><?php //the_field('program_6_text');?></p>
+							</div>
+
+						</div>
+
+					</div><!-- end #programs -->
+
+
+
+
+				</main> <!-- end #main -->
 
 		</div> <!-- end #inner-content -->
 
