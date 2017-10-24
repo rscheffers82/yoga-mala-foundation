f55f5a8bc77907fcd9374e04102a646571e0e5f8
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-24 15:06:25 -0400

    full directory commit of previous commit as some files were missed

--- wp-content/themes/yoga-mala-foundation/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/yoga-mala-foundation/parts/nav-offcanvas-topbar.php
@@ -2,18 +2,18 @@
 	 and a topbar for medium-up -->
 
 <div class="top-bar" id="top-bar-menu">
+	<div class="top-bar-right float-right show-for-small-only">
+		<ul class="menu">
+			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
+		</ul>
+	</div>
 	<div class="top-bar-left float-left">
 		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			<li class="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/ymf-logo.svg'; ?>" alt="Yoga Mala Foundation Logo"/></a></li>
 		</ul>
 	</div>
 	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
+		<?php joints_top_nav(); ?>
 	</div>
 </div>
