3789b609019dbc65feaa9a5a98a4dd6ff36163f5
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-27 15:16:04 -0400

    updates to style, donations page, footer and new image

--- wp-content/themes/yoga-mala-foundation/donations.php
+++ wp-content/themes/yoga-mala-foundation/donations.php
@@ -36,16 +36,31 @@ Template Name: Donation page
 
 </style>
 
-	<div class="donation-header">
-		<div class="left">
-			<h1><?php the_field('header_headline'); ?></h1>
-			<p><?php the_field('header_subline'); ?></p>
-		</div>
-		<div class="right">
-			<!-- Replace the image with Donorbox code -->
-			<img src="http://ymf.wpengine.com/wp-content/uploads/2017/10/donorbox.jpg">
-		</div>
+		<div class="donation-header">
+			<div class="left">
+				<h1><?php the_field('header_headline'); ?></h1>
+				<p><?php the_field('header_subline'); ?></p>
+			</div>
+			<div class="right">
+				<!-- Replace the image with Donorbox code -->
+				<img src="http://ymf.wpengine.com/wp-content/uploads/2017/10/donorbox.jpg">
+			</div>
 
-	</div>
+		</div>
+	</div> <!-- .hero -->
+
+	<div id="content">
+
+		<div id="inner-content" class="row">
+
+		    <main id="main" class="large-8 medium-8 columns" role="main">
+
+
+			</main> <!-- end #main -->
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
 
 <?php get_footer(); ?>
