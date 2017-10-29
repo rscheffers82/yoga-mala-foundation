52b3713d49f1d7ef094ebdb8385d43dc03632a72
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-29 17:05:19 -0400

    more styles, mobile responsive menus

--- wp-content/themes/yoga-mala-foundation/assets/js/scripts.js
+++ wp-content/themes/yoga-mala-foundation/assets/js/scripts.js
@@ -21,4 +21,57 @@ jQuery(document).ready(function() {
     }
   });
 
+  // Adds scrollTo Menu functionality to page sections
+  // let navitems = ['about', 'programs', 'partners', 'involve'];
+  //
+  // for (let i = 0; i<navitems.length; i++) {
+  //
+  //   jQuery('ul#menu-main-1 li.' + i +' a').click(function(e){
+  //       e.preventDefault();
+  //       jQuery('html, body').animate({
+  //        scrollTop: jQuery("div#'+ i + '").offset().top
+  //       }, 1500);
+  //     });
+  // }
+
+
+  // ABout
+  jQuery('ul#menu-main-1 li.about a, ul#menu-main-2 li.about a, ul#menu-main li.about a').click(function(e){
+      e.preventDefault();
+      console.log('clicked!');
+      jQuery('html, body').animate({
+       scrollTop: jQuery("div#inner-content").offset().top
+      }, 1500);
+    });
+
+    // Programs
+    jQuery('ul#menu-main-1 li.programs a, ul#menu-main-2 li.programs a').click(function(e){
+        e.preventDefault();
+        console.log('clicked!');
+        jQuery('html, body').animate({
+         scrollTop: jQuery("div#programs").offset().top
+        }, 1500);
+      });
+
+    // Partners
+    jQuery('ul#menu-main-1 li.partners a, ul#menu-main-2 li.partners a').click(function(e){
+        e.preventDefault();
+        console.log('clicked!');
+        jQuery('html, body').animate({
+         scrollTop: jQuery("div#partners").offset().top
+        }, 1500);
+      });
+
+    // Get Involved
+    jQuery('ul#menu-main-1 li.involve a, ul#menu-main-2 li.involve a').click(function(e){
+        e.preventDefault();
+        console.log('clicked!');
+        jQuery('html, body').animate({
+         scrollTop: jQuery("div#contact").offset().top
+        }, 1500);
+      });
+
+
+
+
 });
