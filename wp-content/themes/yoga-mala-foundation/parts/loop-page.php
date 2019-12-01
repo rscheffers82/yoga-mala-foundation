c148e55b8298e6d773fdfdf97e46147a35e671bf
Andrew Sepic <andrewsepic@Andrews-MacBook-Pro.local>
2019-12-01 10:19:56 -0500

    minor style changes and added custom field toggle to hide page header

--- wp-content/themes/yoga-mala-foundation/parts/loop-page.php
+++ wp-content/themes/yoga-mala-foundation/parts/loop-page.php
@@ -1,18 +1,24 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-						
-	<header class="article-header">
-		<h1 class="page-title"><?php the_title(); ?></h1>
-	</header> <!-- end article header -->
-					
+
+	<?php if( get_field('hide_page_title' )) {
+		// Don't show title
+	}
+	else { ?>
+		<header class="article-header">
+			<h1 class="page-title"><?php the_title(); ?></h1>
+		</header> <!-- end article header -->
+	<?php }?>
+
+
     <section class="entry-content" itemprop="articleBody">
 	    <?php the_content(); ?>
 	    <?php wp_link_pages(); ?>
 	</section> <!-- end article section -->
-						
+
 	<footer class="article-footer">
-		
+
 	</footer> <!-- end article footer -->
-						    
+
 	<?php comments_template(); ?>
-					
+
 </article> <!-- end article -->
